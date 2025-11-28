bdf1db6ea17ade640c4b9f7cb6ab604a6671e078
Matheus <matheuzinhosk8@hotmail.com>
2025-11-28 08:36:18 -0300

    removendo alguns bugs

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,10 @@ import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-d
 import { useState } from "react";
 
 import LoginPage from "./pages/LoginPage";
-import CadastroPage from "./pages/CadastroPage"; // cadastro de usuário
+import CadastroPage from "./pages/CadastroPage";
 import HomePage from "./pages/HomePage";
 import RecipePage from "./pages/RecipePage";
-import CadastroReceitaPage from "./pages/CadastroReceitaPage"; // novo cadastro de receitas
+import CadastroReceitaPage from "./pages/CadastroReceitaPage";
 import Navbar from "./components/Navbar";
 
 export default function App() {
@@ -13,26 +13,26 @@ export default function App() {
 
   return (
     <Router>
-      {/* Navbar só aparece se o usuário estiver logado */}
+
       {logado && <Navbar />}
 
       <Routes>
-        {/* Tela de login só aparece se NÃO estiver logado */}
+
         <Route
           path="/"
           element={
             logado ? (
-              <Navigate to="/home" replace /> // redireciona pra home
+              <Navigate to="/home" replace />
             ) : (
               <LoginPage onLogin={() => setLogado(true)} />
             )
           }
         />
 
-        {/* Tela de cadastro de usuário */}
+
         <Route path="/cadastro" element={<CadastroPage />} />
 
-        {/* Rotas visíveis apenas após login */}
+
         {logado && (
           <>
             <Route path="/home" element={<HomePage />} />
@@ -41,7 +41,7 @@ export default function App() {
           </>
         )}
 
-        {/* Se tentar acessar qualquer rota sem estar logado, volta pro login */}
+
         {!logado && <Route path="*" element={<Navigate to="/" replace />} />}
       </Routes>
     </Router>
